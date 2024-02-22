414ba035afbdb581e6227011f0419d1891220516
Johnson Sun <j3.soon777@gmail.com>
2024-02-23 00:54:31 +0800

    Update isaac sim simulation example to read args and write output to filesystem and Nucleus
    Ref: https://docs.omniverse.nvidia.com/kit/docs/client_library/latest/docs/python.html

--- tasks/isaac-sim-simulation-example.py
+++ tasks/isaac-sim-simulation-example.py
@@ -1,11 +1,22 @@
+import argparse
+import pathlib
+
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("steps", help="number of time steps to simulate", type=int)
+args = parser.parse_args()
+print(args.steps)
+
 #launch Isaac Sim before any other imports
 #default first two lines in any standalone application
 from omni.isaac.kit import SimulationApp
-simulation_app = SimulationApp({"headless": False}) # we can also run as headless.
 
+simulation_app = SimulationApp({"headless": True})
+
+import omni
 from omni.isaac.core import World
 from omni.isaac.core.objects import DynamicCuboid
-import numpy as np
 
 world = World()
 world.scene.add_default_ground_plane()
@@ -20,7 +31,7 @@ fancy_cube =  world.scene.add(
 # Resetting the world needs to be called before querying anything related to an articulation specifically.
 # Its recommended to always do a reset after adding your assets, for physics handles to be propagated properly
 world.reset()
-for i in range(500):
+for i in range(args.steps):
     position, orientation = fancy_cube.get_world_pose()
     linear_velocity = fancy_cube.get_linear_velocity()
     # will be shown on terminal
@@ -30,5 +41,23 @@ for i in range(500):
     # we have control over stepping physics and rendering in this workflow
     # things run in sync
     world.step(render=True) # execute one physics step and one rendering step
+
+output = f"""\
+Simulation steps : {args.steps}
+Cube position is : {position}
+Cube's orientation is : {orientation}
+Cube's linear velocity is : {linear_velocity}
+
+"""
+
+# The results could be written to a file or to Nucleus
+# (1) Write to output file
+pathlib.Path("/results").mkdir(parents=True, exist_ok=True)
+with open("/results/isaac-sim-simulation-example.txt", "w") as file:
+    file.write(output)
+
+# (2) Write to Nucleus
+result = omni.client.write_file("omniverse://localhost/Projects/J3soon/Isaac/2023.1.1/Outputs/isaac-sim-simulation-example.txt", output.encode())
+print(result)
 
 simulation_app.close() # close Isaac Sim
